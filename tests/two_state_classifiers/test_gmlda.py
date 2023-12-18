5624218007ce5a26bc3a0fe457ac4fc28563690d
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-12-18 14:28:42 +0100

    Solve bug

--- tests/two_state_classifiers/test_gmlda.py
+++ tests/two_state_classifiers/test_gmlda.py
@@ -31,19 +31,19 @@ def test_TwoStateLinearClassifierFit():
         np.random.multivariate_normal(mu0, cov, size=M),
     )
 
-    cla = TwoStateLinearClassifierFit().fit(shots_0, shots_1, n_bins=101)
+    cla = TwoStateLinearClassifierFit().fit(shots_0, shots_1)
 
     params = cla.params()
 
-    assert pytest.approx(params["rot_angle"], rel=1e-2) == np.pi / 4
-    assert pytest.approx(params["threshold"], rel=1e-2) == np.sqrt(2) / 2
+    assert pytest.approx(params["rot_angle"], rel=2e-2) == np.pi / 4
+    assert pytest.approx(params["threshold"], rel=2e-2) == np.sqrt(2) / 2
     assert (
-        pytest.approx(params[0]["mu_0"], abs=1e-2) == 0
+        pytest.approx(params[0]["mu_0"], abs=2e-2) == 0
     )  # relative comparison with 0 is not correct
-    assert pytest.approx(params[0]["mu_1"], rel=1e-2) == np.sqrt(2)
-    assert pytest.approx(params[0]["sigma"], rel=1e-2) == np.sqrt(0.3)
-    assert pytest.approx(params[0]["angle"], rel=1e-2) == np.arcsin(np.sqrt(p0))
-    assert pytest.approx(params[1]["angle"], rel=1e-2) == np.arccos(np.sqrt(p1))
+    assert pytest.approx(params[0]["mu_1"], rel=2e-2) == np.sqrt(2)
+    assert pytest.approx(params[0]["sigma"], rel=2e-2) == np.sqrt(0.3)
+    assert pytest.approx(params[0]["angle"], rel=2e-2) == np.arcsin(np.sqrt(p0))
+    assert pytest.approx(params[1]["angle"], rel=2e-2) == np.arccos(np.sqrt(p1))
     return
 
 
@@ -57,7 +57,7 @@ def test_load():
     params = deepcopy(PARAMS)
     params["rot_angle"] = None
     with pytest.raises(ValueError) as e_info:
-        cla = TwoStateLinearClassifierFit().load({})
+        cla = TwoStateLinearClassifierFit().load(params)
     return
 
 
