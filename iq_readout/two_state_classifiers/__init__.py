43a17f889b9bb49e4f2107879d8b949f706115a1
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-02-16 15:12:29 +0100

    Solve typo

--- iq_readout/two_state_classifiers/__init__.py
+++ iq_readout/two_state_classifiers/__init__.py
@@ -2,4 +2,4 @@ from .gmlda import GaussMixLinearClassifier
 from .decaylda import DecayLinearClassifier
 from . import plots_1d
 
-__all__ = ["GaussMixClassifier", "DecayLinearClassifier", "plots_1d"]
+__all__ = ["GaussMixLinearClassifier", "DecayLinearClassifier", "plots_1d"]
