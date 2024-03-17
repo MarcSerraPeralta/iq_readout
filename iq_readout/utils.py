1c3a7af50856f486e1ce4452a069df9a05e08ce8
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-03-17 18:32:45 +0100

    Improve utils.py

--- iq_readout/utils.py
+++ iq_readout/utils.py
@@ -11,27 +11,45 @@ FIT_KARGS = dict(
 
 
 def check_2d_input(x: np.ndarray, axis=-1):
+    """
+    Checks that `x` has length 2 at the last axis.
+    If `axis != -1`, it also checks that `x` has `axis+1` axis.
+
+    Parameters
+    ----------
+    x
+        Input to check.
+    axis
+        Axis of the last dimension fo the input.
+        By default `-1`.
+    """
     if x.shape[axis] != 2:
         raise ValueError(
             "input must be specified with shape (..., 2), "
             f"but the following shape was given: {x.shape}"
         )
-    if axis != -1:
-        if len(x.shape) != axis + 1:
-            raise ValueError(
-                f"input must have {axis+1} axis, "
-                f"but the following shape was given: {x.shape}"
-            )
+    if (axis != -1) and (len(x.shape) != axis + 1):
+        raise ValueError(
+            f"input must have {axis+1} axis, "
+            f"but the following shape was given: {x.shape}"
+        )
+
     return
 
 
 def get_angle(vector: np.ndarray) -> float:
     """
-    The counterclockwise angle from the x-axis in the range (-pi, pi]
+    The counterclockwise angle from the x-axis in the range (-pi, pi].
 
     Parameters
     ----------
     vector: np.ndarray(2)
+        Input vector.
+
+    Returns
+    -------
+    angle
+        Counterclockwise angle of `vector`.
     """
     assert vector.shape == (2,)
 
@@ -41,16 +59,20 @@ def get_angle(vector: np.ndarray) -> float:
 
 def rotate_data(x: np.ndarray, theta: float) -> np.ndarray:
     """
-    Counterclock-wise rotation of x by an angle theta
+    Counterclock-wise rotation of `x` by an angle theta.
 
     Parameters
     ----------
-    x: np.ndarray(N, 2)
+    x: np.ndarray(..., 2)
+        Input data to rotate
     theta: float
+        Angle used to rotate the data.
+        Must be following counterclockwise sign.
 
     Returns
     -------
-    output: np.ndarray(N, 2)
+    output: np.ndarray(..., 2)
+        Rotated data.
     """
     rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
     return np.einsum("ij,...j->...i", rot, x)
