034e218def8712d5fa3172f87a809d0e2b2ed15c
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-06-25 20:17:21 +0200

    Add docs for classifiers in RTD (#36)

--- iq_readout/pdfs.py
+++ iq_readout/pdfs.py
@@ -1,3 +1,10 @@
+"""Module containing the probability density functions used in the classifiers.
+
+The probability density functions have been implemented keeping in mind that
+they will be used for fitting using ``scipy.optimize.curve_fit``, thus all
+its arguments are split into scalars while the input points have been
+grouped into a single multidimensional variable.
+"""
 import numpy as np
 from scipy.special import erf
 
@@ -9,23 +16,23 @@ def simple_1d_gaussian(
     mu: float,
     sigma: float,
 ) -> np.ndarray:
-    """
-    Probability density function of a 1D Gaussian with
-    mean = mu and standard deviation = sigma
+    """Probability density function of a 1D Gaussian.
 
-    Params
-    ------
-    x: np.array(...)
-        Points in the 1D space
+    It has mean ``mu`` and standard deviation ``sigma``.
+
+    Parameters
+    ----------
+    x : np.ndarray(...)
+        Points in the 1D space.
     mu
-        Mean of the first coordinate
+        Mean of the first coordinate.
     sigma
-        Standard deviation of the two coordinates
+        Standard deviation of the two coordinates.
 
     Returns
     -------
-    prob: np.array(...)
-        Values of the probability density function
+    prob : np.ndarray(...)
+        Values of the probability density function.
     """
     prob = 1 / np.sqrt(2 * np.pi * sigma**2) * np.exp(-0.5 * (x - mu) ** 2 / sigma**2)
     return prob
@@ -40,20 +47,29 @@ def simple_1d_gaussian_double_mixture(
 ) -> np.ndarray:
     """
     Probability density function corresponding to the sum of two
-    `simple_1d_gaussian`
+    1D Gaussians with the same standard deviation.  
+
+    It has mean ``mu_0`` and ``mu_1``, and standard deviation ``sigma``.
 
     Parameters
     ----------
-    x: np.array(...)
-        Points in the 1D space
-    mu_i
-        Mean for the i^th Gaussian
+    x : np.ndarray(...)
+        Points in the 1D space.
+    mu_0
+        Mean for the first Gaussian.
+    mu_1
+        Mean for the second Gaussian.
     sigma
-        Standard deviation of the two coordinates for both Gaussians
+        Standard deviation of the two coordinates for both Gaussians.
     angle
-        Weight of the 1st Gaussian is sin(angle)**2 and
-        of the 2nd Gaussian is cos(angle)**2 to ensure that
-        the PDF is normalized
+        Weight of the first Gaussian is sin(angle)**2 and
+        of the second Gaussian is cos(angle)**2 to ensure that
+        the probability density function is normalized.
+
+    Returns
+    -------
+    prob : np.ndarray(...)
+        Values of the probability density function.
     """
     a_0, a_1 = np.sin(angle) ** 2, np.cos(angle) ** 2
 
@@ -72,24 +88,26 @@ def simple_2d_gaussian(
     sigma: float,
 ) -> np.ndarray:
     """
-    Probability density function of a 2D Gaussian with
-    mean = (mu0, mu1) and covariance matrix = diag(sigma**2, sigma**2)
+    Probability density function of a 2D Gaussian. 
 
-    Params
-    ------
-    z: np.array(..., 2)
-        Points in the 2D space
+    It has mean ``(mu_x, mu_y)`` and covariance matrix 
+    ``diag(sigma**2, sigma**2)``.
+
+    Parameters
+    ----------
+    z : np.ndarray(..., 2)
+        Points in the 2D space.
     mu_x
-        Mean of the first coordinate
+        Mean of the first coordinate.
     mu_y
-        Mean of the second coordinate
+        Mean of the second coordinate.
     sigma
-        Standard deviation of the two coordinates
+        Standard deviation of the two coordinates.
 
     Returns
     -------
-    prob: np.array(...)
-        Values of the probability density function
+    prob : np.ndarray(...)
+        Values of the probability density function.
     """
     check_2d_input(z)
     x, y = z[..., 0], z[..., 1]
@@ -109,27 +127,36 @@ def simple_2d_gaussian_double_mixture(
 ) -> np.ndarray:
     """
     Probability density function corresponding to the sum of two
-    `simple_2d_gaussian`
+    2D Gaussians. 
+
+    The two Gaussians have means ``(mu_0_x, mu_0_y)`` and ``(mu_1_x, mu_1_y)``,
+    and same covariance matrix ``diag(sigma**2, sigma**2)``.
+
+    The weight of the first Gaussian is ``sin(angle)**2`` and
+    of the second Gaussian is ``cos(angle)**2`` to ensure that
+    the PDF is normalized.
 
     Parameters
     ----------
-    z: np.array(..., 2)
-        Points in the 2D space
-    mu_i_x
-        Mean of the first coordinate for the i^th Gaussian
-    mu_i_y
-        Mean of the second coordinate for the i^th Gaussian
+    z : np.ndarray(..., 2)
+        Points in the 2D space.
+    mu_0_x
+        Mean of the first coordinate for the first Gaussian.
+    mu_0_y
+        Mean of the second coordinate for the firs Gaussian.
+    mu_1_x
+        Mean of the first coordinate for the second Gaussian.
+    mu_1_y
+        Mean of the second coordinate for the second Gaussian.
     sigma
-        Standard deviation of the two coordinates for both Gaussians
+        Standard deviation of the two coordinates for both Gaussians.
     angle
-        Weight of the 1st Gaussian is sin(angle)**2 and
-        of the 2nd Gaussian is cos(angle)**2 to ensure that
-        the PDF is normalized
+        Weight factor for the Gaussian mixture.
 
     Returns
     -------
-    prob: np.array(...)
-        Values of the probability density function
+    prob : np.ndarray(...)
+        Values of the probability density function.
     """
     check_2d_input(z)
     a_0, a_1 = np.sin(angle) ** 2, np.cos(angle) ** 2
@@ -156,23 +183,39 @@ def simple_2d_gaussian_triple_mixture(
 ) -> np.ndarray:
     """
     Probability density function corresponding to the sum of three
-    `simple_2d_gaussian`
+    2D Gaussians.
+
+    The three Gaussians have means ``(mu_0_x, mu_0_y)``, ``(mu_1_x, mu_1_y)``,
+    and ``(mu_2_x, mu_2_y)``, and same covariance matrix 
+    ``diag(sigma**2, sigma**2)``.
+
+    The weight of the fist Gaussian is ``sin(angle2)**2 * cos(angle1)**2``
+    and of the second Gaussian is ``sin(angle2)**2 * sin(angle1)**2``
+    and of the thrid Gaussian is ``cos(angle2)**2``
+    to ensure that the probability density function is normalized.
 
     Parameters
     ----------
-    z: np.array(..., 2)
+    z : np.ndarray(..., 2)
         Points in the 2D space
-    mu_i_x
-        Mean of the first coordinate for the i^th Gaussian
-    mu_i_y
-        Mean of the second coordinate for the i^th Gaussian
+    mu_0_x
+        Mean of the first coordinate for the first Gaussian.
+    mu_0_y
+        Mean of the second coordinate for the firs Gaussian.
+    mu_1_x
+        Mean of the first coordinate for the second Gaussian.
+    mu_1_y
+        Mean of the second coordinate for the second Gaussian.
+    mu_2_x
+        Mean of the first coordinate for the thrid Gaussian.
+    mu_2_y
+        Mean of the second coordinate for the thid Gaussian.
     sigma
-        Standard deviation of the two coordinates for the i^th Gaussian
-    angle1, angle2
-        Weight of the 1st Gaussian is sin(angle2)**2 * cos(angle1)**2
-        and of the 2nd Gaussian is sin(angle2)**2 * sin(angle1)**2
-        and of the 3rd Gaussian is cos(angle2)**2
-        to ensure that the PDF is normalized
+        Standard deviation of the two coordinates for the Gaussians.
+    angle1
+        Weight factor for the Gaussian mixture.
+    angle2
+        Weight factor for the Gaussian mixture.
     """
     check_2d_input(z)
     a_0, a_1, a_2 = (
@@ -199,23 +242,30 @@ def decay_amplitude_1d_pdf(
     t1_norm: float,
 ):
     """
-    See `decaylda.md`
+    Probability density function observed when considering amplitude decay
+    during the measurement.
 
-    Params
-    ------
-    x
-        Points in the 1D space
-    mu_i
-        Mean for the Gaussian noise for state `i`
+    Parameters
+    ----------
+    x : np.ndarray(...)
+        Points in the 1D space.
+    mu_0
+        Mean for the Gaussian noise for state ``0``.
+    mu_1
+        Mean for the Gaussian noise for state ``1``.
     sigma
-        Standard deviation of the Gaussian noise for state `i`
+        Standard deviation of the Gaussian noise for both states.
     t1_norm
-        T1 normalized with respect to the measurement duration
+        T1 normalized with respect to the measurement duration.
 
     Returns
     -------
-    prob
-        Values of the probability density function
+    prob : np.ndarray(...)
+        Values of the probability density function.
+
+    Notes
+    -----
+    See ``decaylda.md``.
     """
     a_0, a_1 = np.sin(angle) ** 2, np.cos(angle) ** 2
     C = np.sign(mu_1 - mu_0) * (mu_0 - x) / (np.sqrt(2) * sigma) + sigma / (
@@ -253,7 +303,7 @@ def decay_amplitude_2d_pdf(
 
     Parameters
     ----------
-    z: np.array(..., 2)
+    z : np.ndarray(..., 2)
         Points in the 2D space
     mu_i_x
         Mean of the first coordinate for the i^th Gaussian
@@ -270,7 +320,7 @@ def decay_amplitude_2d_pdf(
 
     Returns
     -------
-    prob: np.array(...)
+    prob : np.ndarray(...)
         Values of the probability density function
     """
     check_2d_input(z)
@@ -304,7 +354,7 @@ def pdf_from_hist1d(x, bins, pdf_values):
 
     Parameters
     ----------
-    x: np.array(...)
+    x : np.ndarray(...)
         Points in the 1D space.
     bins: np.ndarray(n_bins)
         Centers of the bins of the 1D histogram.
@@ -326,7 +376,7 @@ def pdf_from_hist2d(z, bins_x, bins_y, pdf_values):
 
     Parameters
     ----------
-    z: np.array(..., 2)
+    z : np.ndarray(..., 2)
         Points in the 2D space.
     bins_x: np.ndarray(n_bins_x)
         Centers of the X-axis bins of the 2D histogram.
